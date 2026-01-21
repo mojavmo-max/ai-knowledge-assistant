f58464a1c864eb74d504294ba3077e006d3f979c
mojavmo-max <mojavmo@gmail.com>
2026-01-21 19:10:22 +1300

    update cors

--- api.py
+++ api.py
@@ -32,7 +32,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://ai-knowledge-assistant-tau.vercel.app/"],
+    "https://ai-knowledge-assistant-tau.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
